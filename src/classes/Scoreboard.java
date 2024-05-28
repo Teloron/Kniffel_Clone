9a341ba7627eb1112a34652419fee73152ef123e
Teloron <dominic.faeth@gmail.com>
2024-05-28 15:44:43 +0200

    Progress

--- src/classes/Scoreboard.java
+++ src/classes/Scoreboard.java
@@ -1,17 +1,15 @@
 package classes;
 
 public class Scoreboard {
-    
+    public static void printScoreOrPlaceholder(int score){
+        
+    }
 
 
 
     public static void printScoreboard(){
 
         
-
-        for (Player player : Player.players){
-            System.out.println(player.getName());
-        }
         
 
         System.out.printf("                     _______________________ _______________________ _______________________ _______________________  \n");
@@ -19,11 +17,9 @@ public class Scoreboard {
         System.out.printf("                    |   %-19s |   %-19s |   %-19s |   %-19s | \n"
         , Player.players.get(0).getName(), Player.players.get(1).getName(), Player.players.get(2).getName(), Player.players.get(3).getName());
 
-        System.out.printf("                    |                       |                       |                       |                       | \n");
-
-
-        System.out.printf("                     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  \n");
-        System.out.printf("  __________________ _______________________ _______________________ _______________________ _______________________  \n");
+        //System.out.printf("                    |                       |                       |                       |                       | \n");
+        //System.out.printf("                     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾  \n");
+        System.out.printf("  __________________|_______________________|_______________________|_______________________|_______________________| \n");
         System.out.printf(" | 1 Nur Einsen     |");
 
         
